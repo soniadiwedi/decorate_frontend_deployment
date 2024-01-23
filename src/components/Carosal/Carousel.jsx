5e115e281bb76c60c534fc45877f71ae004ef64a
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2024-01-23 19:45:21 +0530

    done

---
+++ src/components/Carosal/Carousel.jsx
@@ -0,0 +1,31 @@
+// Carousel.js
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './Carousel.css';  
+
+const Carousel = ({ images }) => {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+  
+    return (
+      <div className="carousel-container">
+        <Slider {...settings}>
+          {images.map((image, index) => (
+            <div key={index}>
+              <img src={image} alt={`Slide ${index + 1}`} />
+            </div>
+          ))}
+        </Slider>
+      </div>
+    );
+  };
+  
+
+export default Carousel;
